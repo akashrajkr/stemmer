3db892b2cb58f28babc22ce61f92a58386e96d56
Akash raj K R <aakashrajkr@gmail.com>
2019-08-13 01:17:32 +0530

    finder.java finds root words and outputs a list of meaningful stemmed words for that root word

--- README.md
+++ README.md
@@ -8,3 +8,36 @@
 * root* has the root words
 * suffix* file has the suffixes
 * words_alpha.txt: https://github.com/dwyl/english-words/blob/master/words_alpha.txt
+
+## Sample output as of now
+Enter a word:
+ 
+understand
+
+Root Word: understand
+
+=================================
+
+Pure Prefixes are :
+
+misunderstand
+
+=================================
+
+Pure Suffixes are :
+
+understandable
+
+understanding
+
+=================================
+
+Mixed words are: 
+
+misunderstandable
+
+misunderstanding
+
+misunderstandingly
+
+understandability
--- src/finder.java
+++ src/finder.java
@@ -1,3 +1,91 @@
-public class finder {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class finder {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean isRoot = false;
+        String root = null;
+        String[] suffixCodes = new String[0];
+        String[] prefixCodes = new String[0];
+        System.out.println("Enter a word: ");
+        String word = null;
+        while (!isRoot) {
+            word = s.nextLine();
+            try (BufferedReader reader = new BufferedReader(new FileReader("datasets/root.txt"))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (word.contains(line.toLowerCase())) {
+                        System.out.println("Root Word: "+ line);
+                        root = line.toLowerCase();
+                        isRoot = true;
+                    }
+                }
+                if (!isRoot) System.out.println("Please enter a root word: ");
+            }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+        ArrayList<String> dictArray = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("datasets/dict.txt"))) {
+            String fileword;
+            while((fileword = reader.readLine()) != null){
+                if(fileword.contains(root)){
+                    dictArray.add(fileword);
+                }
+            }
+            dictArray.remove(root);
+            BufferedReader prefixReader = new BufferedReader(new FileReader("datasets/prefix.txt"));
+            prefixCodes = prefixReader.readLine().split(" ");
+            BufferedReader suffixReader = new BufferedReader(new FileReader("datasets/suffix.txt"));
+            suffixCodes = suffixReader.readLine().split(" ");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        prefixCodes, suffixCodes and dictArray
+//        Check for prefixes
+        ArrayList<String> prefixWords = new ArrayList<>();
+        ArrayList<String> suffixWords = new ArrayList<>();
+        ArrayList<String> mixedWords = new ArrayList<>();
+        System.out.println("\n=================================\n");
+        System.out.println("Pure Prefixes are :");
+        for (int i = 0; i < prefixCodes.length;  i++) {
+            if(dictArray.contains(prefixCodes[i]+root)){
+                System.out.println(prefixCodes[i]+root);
+                prefixWords.add(prefixCodes[i]+root);
+            }else{
+                mixedWords.add(dictArray.get(i));
+            }
+        }
+//       Check for suffixes
+        System.out.println("\n=================================\n");
+        System.out.println("Pure Suffixes are :");
+        for (int i = 0; i < suffixCodes.length;  i++) {
+            String temp = root + suffixCodes[i];
+            if(dictArray.contains(temp)){
+                System.out.println(temp);
+                suffixWords.add(temp);
+                if(mixedWords.contains(temp)){
+                    mixedWords.remove(temp);
+                }
+            } else{
+                if(!prefixWords.contains(dictArray.get(i)))
+                    if(!mixedWords.contains(dictArray.get(i)))
+                        mixedWords.add(dictArray.get(i));
+            }
+        }
+        System.out.println("\n=================================\n");
+        System.out.println("Mixed words are: ");
+        for (int i = 0; i < mixedWords.size(); i++) {
+            System.out.println(mixedWords.get(i));
+        }
+    }
 }
